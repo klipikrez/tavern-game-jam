d9a827faf39c94fd35de547d47b6b7d34486722d
Pavoximus Studio <143937539+pavoximus-studio@users.noreply.github.com>
2024-03-17 14:26:35 +0100

    Flip objects

---
+++ Assets/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public  GameObject[] objects; //objekti koje player postavlja
+    private bool flipp = false; //flipp true rotira po y, false po x
+
+    void Mirror()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            Vector2 flip = objects[i].transform.localScale;
+            Vector2 curretPos = objects[i].transform.position;
+            Vector2 mirroredPos;
+            if (!flipp)
+            {
+                mirroredPos = new Vector2(-curretPos.x, curretPos.y);
+                flip.x *= -1;
+            }
+            else
+            {
+                mirroredPos = new Vector2(curretPos.x, -curretPos.y);
+                flip.y *= -1;
+            }
+            
+            objects[i].transform.position = mirroredPos;
+            objects[i].transform.localScale = flip;
+        }
+        bool x = !flipp;
+        flipp = x;
+    }
+    void NewLevel()
+    {
+        //place new object
+        //Mirror()
+    }
+    void MovePortal()
+    {
+        //pomeriti portale po y osi u odredjenim granicama
+    }
+    void PlaceItem()
+    {
+        //postaviti item u dozvoljenom prostoru
+    }
+    void Start()
+    {
+        Mirror();//test
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
