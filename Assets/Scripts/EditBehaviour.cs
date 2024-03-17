a1b4b15eec1705ca5083c01302c986d1e25e437b
klipik_rez <78925089+klipikrez@users.noreply.github.com>
2024-03-17 18:41:17 +0100

    portal

--- Assets/Scripts/EditBehaviour.cs
+++ Assets/Scripts/EditBehaviour.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using static Functions;
 
 [CreateAssetMenu(fileName = "new editor beh", menuName = "gameManager/editor")]
 public class EditBehaviour : BaseBehaviour
 {
     public bool placePortals = true;
     public int currentPortal = 0;
+    SpriteRenderer portalSprite;
     public override void StartBehaviour(GameManager manager)
     {
         currentPortal = 0;
         placePortals = true;
+        if (portalSprite == null)
+        {
+            portalSprite = manager.portals[0].transform.Find("spawn").GetComponent<SpriteRenderer>();
+        }
     }
 
     public override void UpdateBehaviour(GameManager manager)
     {
         if (placePortals)
         {
+            if (currentPortal == 0)
+            {
+                portalSprite.color = Color.white;
+                DrawBox(manager.portals[0].transform.position + new Vector3(0.5f, 0, 0), quaternion.identity, Vector2.one, Color.red);
+                Collider2D[] colliders = Physics2D.OverlapBoxAll(manager.portals[0].transform.position + new Vector3(0.5f, 0, 0), Vector2.one, 0f);
+                foreach (Collider2D col in colliders)
+                {
+                    if (col != null && col.gameObject.layer != LayerMask.NameToLayer("Player"))
+                    {
+                        portalSprite.color = Color.red;
+                    }
+                }
+            }
             manager.portals[currentPortal].transform.position = new Vector3(manager.portals[currentPortal].transform.position.x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         }
         if (Input.GetMouseButtonDown(0))
--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -51,4 +51,38 @@ public static class Functions
         //ovo ti radi stvar, tako da lerp zavisi od vremena, a ne da vraca samo fiksnu vrednost svakog frejma
         return 1 - Mathf.Pow(1 - value, Time.deltaTime * 60);
     }
+    public static void DrawBox(Vector3 pos, Quaternion rot, Vector3 scale, Color c)
+    {
+        // create matrix
+        Matrix4x4 m = new Matrix4x4();
+        m.SetTRS(pos, rot, scale);
+
+        var point1 = m.MultiplyPoint(new Vector3(-0.5f, -0.5f, 0.5f));
+        var point2 = m.MultiplyPoint(new Vector3(0.5f, -0.5f, 0.5f));
+        var point3 = m.MultiplyPoint(new Vector3(0.5f, -0.5f, -0.5f));
+        var point4 = m.MultiplyPoint(new Vector3(-0.5f, -0.5f, -0.5f));
+
+        var point5 = m.MultiplyPoint(new Vector3(-0.5f, 0.5f, 0.5f));
+        var point6 = m.MultiplyPoint(new Vector3(0.5f, 0.5f, 0.5f));
+        var point7 = m.MultiplyPoint(new Vector3(0.5f, 0.5f, -0.5f));
+        var point8 = m.MultiplyPoint(new Vector3(-0.5f, 0.5f, -0.5f));
+
+        Debug.DrawLine(point1, point2, c);
+        Debug.DrawLine(point2, point3, c);
+        Debug.DrawLine(point3, point4, c);
+        Debug.DrawLine(point4, point1, c);
+
+        Debug.DrawLine(point5, point6, c);
+        Debug.DrawLine(point6, point7, c);
+        Debug.DrawLine(point7, point8, c);
+        Debug.DrawLine(point8, point5, c);
+
+        Debug.DrawLine(point1, point5, c);
+        Debug.DrawLine(point2, point6, c);
+        Debug.DrawLine(point3, point7, c);
+        Debug.DrawLine(point4, point8, c);
+
+
+    }
+
 }
