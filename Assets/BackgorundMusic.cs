561bf45c6983becc43e52432488a495ca7eb5bfa
klipik_rez <78925089+klipikrez@users.noreply.github.com>
2024-03-18 23:50:02 +0100

    zvuk

--- Assets/BackgorundMusic.cs
+++ Assets/BackgorundMusic.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class BackgorundMusic : MonoBehaviour
 {
+    public string name;
     // Start is called before the first frame update
     void Start()
     {
-        //AudioManager.Instance.PlayAudioClipLooping("MainMenu");
+        AudioManager.Instance.PlayAudioClipLooping(name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
